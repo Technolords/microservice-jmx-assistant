b3cc7cfa5b70a6b53605cdb3507e14043cdd25eb
Technolords <mike@technolords.net>
2017-02-27 14:49:53 +0100

    Bean with Handler annotation introduced to support the splitter (by host)

---
+++ src/main/java/net/technolords/micro/camel/expression/HostSplitter.java
@@ -0,0 +1,23 @@
+package net.technolords.micro.camel.expression;
+
+import java.util.List;
+
+import org.apache.camel.Handler;
+import org.apache.camel.Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.technolords.micro.camel.JolokiaMain;
+import net.technolords.micro.model.QueryContext;
+import net.technolords.micro.model.jaxb.Host;
+
+public class HostSplitter {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Handler
+    public List<Host> splitByHost(@Header(value = JolokiaMain.HEADER_QUERY_CONTEXT) QueryContext queryContext) {
+        LOGGER.trace("Splitter by Host called...");
+        return queryContext.getHosts();
+    }
+
+}
