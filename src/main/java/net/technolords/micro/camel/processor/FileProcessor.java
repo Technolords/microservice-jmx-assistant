077c2aaf6b3cc5bf3545a92bef5a3623d6602be6
Technolords <mike@technolords.net>
2017-03-01 13:03:19 +0100

    Extended model for FileOutput to hold filename and mode

--- src/main/java/net/technolords/micro/camel/processor/FileProcessor.java
+++ src/main/java/net/technolords/micro/camel/processor/FileProcessor.java
@@ -2,14 +2,35 @@ package net.technolords.micro.camel.processor;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.jolokia.client.request.J4pResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.technolords.micro.camel.JolokiaMain;
+import net.technolords.micro.model.jaxb.Output;
+import net.technolords.micro.model.jaxb.output.FileOutput;
+
 public class FileProcessor implements Processor {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static final String APPEND = "append";
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        LOGGER.info("Todo: not implemented yet...");
+        Output output = exchange.getProperty(JolokiaMain.PROPERTY_OUTPUT, Output.class);
+        J4pResponse response = exchange.getIn().getHeader(JolokiaMain.HEADER_RESPONSE, J4pResponse.class);
+        this.writeToFile(output.getFileOutput(), response);
     }
+
+    private void writeToFile(FileOutput fileOutput, J4pResponse response) {
+        if (response != null) {
+            LOGGER.info("Got output, about to write to file...");
+//            if (fileOutput)
+        }
+    }
+
+    /*
+        Path path = FileSystems.getDefault().getPath(pathToJolokiaConfig);
+        Files.newInputStream(path, StandardOpenOption.READ);
+        LOGGER.info("Response: {}", response.asJSONObject().toJSONString());
+     */
 }
--- src/main/java/net/technolords/micro/model/jaxb/output/FileOutput.java
+++ src/main/java/net/technolords/micro/model/jaxb/output/FileOutput.java
@@ -1,4 +1,27 @@
 package net.technolords.micro.model.jaxb.output;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
+
 public class FileOutput {
+    private String file;
+    private String mode;
+
+    @XmlValue
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    @XmlAttribute (name = "mode")
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
 }
